caa39d6dbb481f302abcf073e16b73935c1f3783
Gian Villaronte <gianvillaronte@gmail.com>
2023-10-30 20:23:19 -0400

    10-30-2023

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Contracts;
 using LoggerService;
 using Repository;
+using Service.Contracts;
+using Service;
+using Microsoft.EntityFrameworkCore;
 
 namespace CompanyEmployees.ServiceExtensions;
 
@@ -17,15 +20,21 @@ public static class ServiceExtensions
 
 
     public static void ConfigureIISIntegration(this IServiceCollection services) =>
-     services.Configure<IISOptions>(options =>
-     {
-     });
+     services.Configure<IISOptions>(options => {});
 
     public static void ConfigureLoggerService(this IServiceCollection services) =>
          services.AddSingleton<LoggerService.ILoggerManager, LoggerManager>();
 
     public static void ConfigureRepositoryManager(this IServiceCollection services) =>
- services.AddScoped<IRepositoryManager, RepositoryManager>();
+        services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+    public static void ConfigureServiceManager(this IServiceCollection services) =>
+        services.AddScoped<IServiceManager, ServiceManager>();
+
+    public static void ConfigureSqlContext(this IServiceCollection services,
+        IConfiguration configuration) =>
+        services.AddDbContext<RepositoryContext>(opts =>
+        opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 
 
 
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -11,8 +11,11 @@ builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+ 
+builder.Services.AddControllers().AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
-builder.Services.AddControllers();
 
 var app = builder.Build();
 
